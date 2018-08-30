d9032a48173de74c114c84626d546f1600377236
Tenzin Migmar <tenzinmigmar@Tenzins-iMac.local>
2018-08-30 12:30:34 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -74,15 +74,23 @@ var cards = [
 var cardsInPlay = [];
 
 
+var checkForMatch = function(){
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+} else {
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+	if (cardsInPlay.length == 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again");
 	}
+}
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribut('data-id');
+	this.scr = console.log(cards[cardId].cardImage);
 	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("User flipped " + cards[cardId].rank);
 	}
@@ -93,14 +101,26 @@ var flipCard = function(cardId) {
 	checkForMatch();
 };
 
-flipCard(cards[cardId] = 0);
+//flipCard(cards[cardId] = 0);
 
-flipCard(cards[cardId] = 2);
+//flipCard(cards[cardId] = 2);
 
 cardInplay.push(cards[cardId].rank);
 
-console.log(card[cardId].cardImage);
-console.log(card[cardId].suit);
+var createBoard =function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addElementListener('click', flipCard);
+		document.getElementsById('game-board').appendChild(cardElement);
+	}
+}
+
+//console.log(card[cardId].cardImage);
+//console.log(card[cardId].suit);
+
+createBoard();
 
 
 
